72e0b84710acbdedb6ab722e34d97be0dcd2a131
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2025-04-16 18:59:07 +0700

    Update run_windows_builder.bat
    - Auto-install required dependency
    - Change license to GPLv3

--- derdos_builder/build_windows/run_windows_builder.bat
+++ derdos_builder/build_windows/run_windows_builder.bat
@@ -1,8 +1,29 @@
 @echo off
 
+REM Check if Nuitka is installed
+echo Checking for Nuitka installation...
+python -c "import nuitka" 2>nul
+if %errorlevel% neq 0 (
+    echo Nuitka is not installed. Installing now...
+    pip install nuitka
+) else (
+    echo Nuitka is already installed.
+)
+
+REM Check if PyQt5 is installed
+echo Checking for PyQt5 installation...
+python -c "import PyQt5" 2>nul
+if %errorlevel% neq 0 (
+    echo PyQt5 is not installed. Installing now...
+    pip install PyQt5
+) else (
+    echo PyQt5 is already installed.
+)
+echo.
+
 REM Step 1: Run nuitka to build main.exe
 echo 1. Running nuitka to build main.exe...
-python -m nuitka --standalone ..\..\main.py --onefile --enable-plugin=pyqt5 --windows-icon-from-ico="..\asset\windows-logo.ico" --windows-console-mode=disable --company-name="ATOMIC09" --product-name="DerDos" --file-version=1.1 --product-version=1.1 --file-description="A Simple Denial of Service Tool with GUI" --copyright="Licensed under the MIT License"
+python -m nuitka --standalone ..\..\main.py --onefile --enable-plugin=pyqt5 --windows-icon-from-ico="..\asset\windows-logo.ico" --windows-console-mode=disable --company-name="ATOMIC09" --product-name="DerDos" --file-version=1.1 --product-version=1.1 --file-description="A Simple Denial of Service Tool with GUI" --copyright="Licensed under the GPLv3 License"
 if %errorlevel% neq 0 (
     echo ! Nuitka failed to build the application.
     exit /b 1
